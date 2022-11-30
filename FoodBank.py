607ddddf33b489f5f91bfef88c993f886975e9fe
Rodrigo Morais <rmoraisu@protonmail.com>
2022-11-30 13:17:51 -0600

    comments for #1

--- FoodBank.py
+++ FoodBank.py
@@ -7,3 +7,9 @@ class FoodBank:
     num_pantries = int(.5 * food_insecure_pop / ppl_per_pantry)
     self.pantries = [FoodPantry(self) for i in range(num_pantries)]
     # set proportion of pantries from food_insecure
+
+
+    # predicted supply and predicted demand
+    # open to receiving food
+    # food available to donate
+    # feeding america tax
