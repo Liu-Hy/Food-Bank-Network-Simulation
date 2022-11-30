9cc779574cfe60f7c8a08951261d0af93607ace3
Rodrigo Morais <rmoraisu@protonmail.com>
2022-11-30 12:38:48 -0600

    food pantry number rework for #1

--- FoodBank.py
+++ FoodBank.py
@@ -1,8 +1,9 @@
 import FoodPantry
 
 class FoodBank:
-  def __init__(self, num_pantries: int, per_capita_wealth: float):
+  def __init__(self, food_insecure_pop: int):
+    ppl_per_pantry =  260
+    # we assume half of the food insecure people actually use the bank
+    num_pantries = int(.5 * food_insecure_pop / ppl_per_pantry)
     self.pantries = [FoodPantry(self) for i in range(num_pantries)]
-    self.wealth = per_capita_wealth
-
-
+    # set proportion of pantries from food_insecure
