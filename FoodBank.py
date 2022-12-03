d41c85fcdbb3cfa8eadc9417f632cae777a30f4d
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-12-03 15:30:26 -0600

    FoodBank #1 run day setup

--- FoodBank.py
+++ FoodBank.py
@@ -1,14 +1,21 @@
 import FoodPantry
 import pandas as pd
 from utils import Food
+from typing import List
+
 
 class FoodBank:
   def __init__(self, food_insecure_pop: int):
+
+    # we estimated this number from real data of the Eastern Food Bank
     ppl_per_pantry =  260
     # we assume half of the food insecure people actually use the bank
     num_pantries = int(.5 * food_insecure_pop / ppl_per_pantry)
-    self.pantries = [FoodPantry(self) for i in range(num_pantries)]
-    # set proportion of pantries from food_insecure
+    self.pantries:List(FoodPantry) = [FoodPantry(self) for _ in range(num_pantries)]
+    self.total_utility = None
+    self.total_waste = None
+
+    self._storage = Food()
 
   def food_storage(self) -> pd.DataFrame:
     """Returns food storage stats as a dataframe
@@ -16,12 +23,24 @@ class FoodBank:
     :return: storage dataframe copy
     """
     return self._storage.df.copy()
+  
+  def run_one_day(self, budget: float, food_donations):
+    """Runs simulation for the day. Also calls `run_one_day` for each pantry it serves.
 
-  def receive_dotations(self, amount: float):
-    """Receives donations and allocates it to buying specific products
-
-    :param amount: amount of donations in dollars
+    :param budget: Budget for the day
+    :param food_donations: Budget for the day
     """
+    for pantry in self.pantries:
+      waste, order, utility = pantry.run_one_day()
+      self.total_utility = FoodBank.increment_var(self.total_utility, utility)
+      self.total_waste = FoodBank.increment_var(self.total_waste, waste)
+
+  @classmethod
+  def increment_var(self, var: float, increment: float):
+    if var is None:
+      return increment
+    else:
+      return var + increment
   
   def buy_food(self, donations: float):
     """Buys food based on donations and demand from pantries.
