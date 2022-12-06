6bf457a3f691eaebcd75d560f1bc1d31208decd2
lucianli123 <lucianli123@gmail.com>
2022-12-05 19:15:49 -0600

    script to calculate price history for food categories and gas

---
+++ run_simulation.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import constants
+
+def parse_foodbank_data(food_bank_path: str) -> pd.DataFrame:
+    """
+
+    :param food_bank_path:
+    :return: dataframe of food banks
+    """
+
+
+def parse_price_data(price_path:str)->None:
+    """
+
+    :param price_path:
+    :return: 
+    """
+def initialize_food_banks(food_bank_df: str)->list:
+    """
+
+    :param food_bank_df:
+    :return:
+    """
+def generate_funds_distribution(annual_budget:float):
+    """
+
+    :param annual_budget:
+    :return:
+    """
+
+def generate_food_distribution(annual_pounds_food:float):
+    """
+
+    :param annual_pounds_food:
+    :return:
+    """
+def generate_price_distribution(avg_price:float):
+    """
+
+    :param avg_price:
+    :type avg_price:
+    :return:
+    :rtype:
+    """
+
+def tick_day(food_banks:list):
+    """
+
+    :return:
+    :rtype:
+    """
+def redistribute_food(food_banks:list):
+    """
+
+    :return:
+    :rtype:
+    """
+def generate_distance_matrix(food_banks:list):
+    """
+
+    :param food_banks:
+    :return:
+    """
