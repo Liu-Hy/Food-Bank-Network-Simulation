2cc30ced7d3d16de51f1b6097c05a9a6693c1230
lucianli123 <lucianli123@gmail.com>
2022-12-06 10:44:49 -0600

    minor notebook changes

--- run_simulation.py
+++ run_simulation.py
@@ -10,8 +10,9 @@ def initialize_food_banks(food_bank_df: pd.DataFrame)->list[FoodBank]:
     :param food_bank_df: dataframe with food bank data per row
     :return: list of food_banks initialized with pre-generated input data
     >>> input_csv=pd.read_csv("input.csv").head(1)
-    >>> initialize_food_banks(input_csv)[0]._storage
-    blah
+    >>> len(initialize_food_banks(input_csv)[0].pantries) #(323340/2)/245 = 659 rounded down
+    659
+
 
     """
     banks=[]
@@ -26,6 +27,8 @@ def generate_distance_matrix(food_bank_df: pd.DataFrame)->np.ndarray:
     :param food_bank_df: food_bank_df: dataframe with food bank locations
     :return: array of distances (km) between food bank pairs (foodbank x foodbank)
     """
+    
+
 
 def generate_funds_distribution(food_bank_df: pd.DataFrame, num_days:int)-> np.ndarray:
     """
